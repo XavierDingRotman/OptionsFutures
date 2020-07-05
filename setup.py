6e6364d420bc8e9742ad7121f51e187b469a284e
XavierDingRotman <62396188+XavierDingRotman@users.noreply.github.com>
2020-07-05 00:24:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='OptionsFutures',
-    version='1.0',
+    version='1.0-beta',
     packages=find_packages(exclude=['test', 'data', 'analysis']),
     long_description=long_description,
     url='https://github.com/XavierDingRotman/OptionsFutures',
