bab0de0d66efe39f05e9ddf59460ec76547d9ada
XavierDingRotman <62396188+XavierDingRotman@users.noreply.github.com>
2020-07-29 13:56:58 -0400

    Changed name from payment_rate to swap_rate in swap
    Signed-off-by: XavierDingRotman <62396188+XavierDingRotman@users.noreply.github.com>

--- opfu/swap.py
+++ opfu/swap.py
@@ -22,12 +22,12 @@ class FloatingLeg(ForwardRateCurvePaymentFixedIntervalBond):
 
 
 class Swap(Security):
-    def __init__(self, payment_rate, time_to_mature, forward_curve, discount_curve, is_short=False, payment_period=0.5,
+    def __init__(self, swap_rate, time_to_mature, forward_curve, discount_curve, is_short=False, payment_period=0.5,
                  exchange_principle=False, principle=10e6, name="Unnamed Swap", market_price=None):
         self.payment_period = payment_period
         self.principle = principle
         self.is_short = is_short
-        self.fix_leg = FixedLeg(payment_rate, principle, time_to_mature, discount_curve, is_short=(not self.is_short),
+        self.fix_leg = FixedLeg(swap_rate, principle, time_to_mature, discount_curve, is_short=(not self.is_short),
                                 payment_period=payment_period, pay_principle=exchange_principle,
                                 name='Fixed leg of {}'.format(name))
         self.floating_leg = FloatingLeg(forward_curve, principle, time_to_mature, discount_curve,
--- test/swap.py
+++ test/swap.py
@@ -4,7 +4,7 @@ from opfu.swap import Swap
 if __name__ == '__main__':
     libor_curve = SpotRateCurve(function=lambda x: x / 100)
     ois_curve = SpotRateCurve(function=lambda x: x * x / 100)
-    swap = Swap(payment_rate=0.01 / 100, time_to_mature=3, forward_curve=libor_curve.get_forward_rate_curve(),
+    swap = Swap(swap_rate=0.01 / 100, time_to_mature=3, forward_curve=libor_curve.get_forward_rate_curve(),
                 discount_curve=ois_curve)
     print(swap.price)
     print(swap)
