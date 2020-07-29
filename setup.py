fe6783299f3502c9c0e9bb1ed34af642e37d0805
XavierDingRotman <62396188+XavierDingRotman@users.noreply.github.com>
2020-07-29 13:05:50 -0400

    Updated version to 2.0
    Signed-off-by: XavierDingRotman <62396188+XavierDingRotman@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='OptionsFutures',
-    version='1.1',
+    version='2.0',
     packages=find_packages(exclude=['test', 'data', 'analysis']),
     long_description=long_description,
     url='https://github.com/XavierDingRotman/OptionsFutures',
